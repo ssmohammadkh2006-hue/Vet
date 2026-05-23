5a8c91f8b7401e80d49fbbb778db54dfeb52ce64
Mohammad_ALnimrawi <ssmohammad.kh2006@gmail.com>
2026-05-23 22:16:54 +0300

    fix cloudinary storage django 5

--- Vet/settings.py
+++ Vet/settings.py
@@ -148,8 +148,14 @@ MEDIA_URL='/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 CLOUDINARY_STORAGE = {
