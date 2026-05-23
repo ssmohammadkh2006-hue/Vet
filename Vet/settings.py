e6f8861db611858ab9bd9d9c2b20b4639457e8b9
Mohammad_ALnimrawi <ssmohammad.kh2006@gmail.com>
2026-05-24 00:14:19 +0300

    fix settings production rendffffer

--- Vet/settings.py
+++ Vet/settings.py
@@ -128,7 +128,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.WhiteNoiseStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
