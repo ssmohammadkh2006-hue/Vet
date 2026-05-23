e988739b0d60e31f9c2690adbc9e9e5f1765e870
Mohammad_ALnimrawi <ssmohammad.kh2006@gmail.com>
2026-05-24 01:30:16 +0300

    fix settings css

--- Vet/settings.py
+++ Vet/settings.py
@@ -98,7 +98,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # =========================
