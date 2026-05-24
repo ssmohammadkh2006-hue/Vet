f1f08060f21a33407e436fc200332ad608d9f54e
Mohammad_ALnimrawi <ssmohammad.kh2006@gmail.com>
2026-05-24 14:32:23 +0300

    fix media and css static two

--- Vet/settings.py
+++ Vet/settings.py
@@ -100,7 +100,17 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -117,7 +127,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'ZSEX2',
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 
  
